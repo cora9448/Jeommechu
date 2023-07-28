679d8d507f11937ea9826e80b5a499626a93547b
Hee Seo Kim <hisounni@ewhain.net>
2023-07-28 12:15:35 +0900

    7/28

--- main.js
+++ main.js
@@ -18,12 +18,29 @@ function getRandomMenu() {
     // 결과를 웹 페이지에 표시
     const resultElement = document.getElementById("result");
     resultElement.textContent = "" + selectedMenus.join(", ");
+
+    // 선택된 메뉴를 화면에 표시
+    const menuContainer = document.getElementById("menu-container");
+    menuContainer.innerHTML = ""; // 기존에 표시된 메뉴들 초기화
+
+    for (const menu of selectedMenus) {
+        const menuElement = document.createElement("div");
+        menuElement.textContent = menu;
+        menuElement.classList.add("menu-item");
+        menuElement.addEventListener("click", () => voteForMenu(menu)); // 음식 클릭 시 투표 함수 호출
+        menuContainer.appendChild(menuElement);
+    }
 }
 
-// 각 페이지를 보여주는 함수
-function showPage(pageNumber) {
-    // 여기서 각 페이지에 대한 로직을 구현합니다.
-    // 이 예시에서는 단순히 페이지 번호만 출력합니다.
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = "현재 보고 있는 페이지: " + pageNumber;
+// 투표 함수
+function voteForMenu(menu) {
+    fetch(`/vote/${menu}`, { method: "POST" }) // 서버에 POST 요청으로 음식 투표 전송
+        .then((response) => response.json())
+        .then((data) => {
+            alert(`${menu} 투표가 완료되었습니다!`);
+        })
+        .catch((error) => {
+            console.error("투표 오류:", error);
+            alert("투표 중 오류가 발생했습니다. 다시 시도해주세요.");
+        });
 }
